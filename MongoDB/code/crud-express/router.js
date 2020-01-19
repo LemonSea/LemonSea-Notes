98cd9285e84a0f9345ce284c5cfebd05ae453ba9
LemonSea <2533566560@qq.com>
2020-01-19 17:55:42 +0800

    mongoose 添加与查询

--- MongoDB/code/crud-express/router.js
+++ MongoDB/code/crud-express/router.js
@@ -7,6 +7,7 @@
 let fs = require('fs')
 let express = require('express')
 let fsHelp = require('./fsHelp')
+let Student = require('./student');
 
 // 使用 express 的路由模块
 // 创建一个路由容器
@@ -21,7 +22,7 @@ router.get('/', function (req, res) {
 
 // 渲染 students 首页
 router.get('/students', function (req, res) {
-    fsHelp.findAll((err, students) => {
+    Student.find((err, students) => {
         if (err) {
             return res.status(500), send('Server error.')
         }
@@ -50,7 +51,7 @@ router.post('/students/add', (req, res) => {
      * 2. 处理
      * 3. 发送响应
      */
-    fsHelp.save(req.body, (err) => {
+    new Student(req.body).save((err) => {
         if (err) {
             console.log('保存失败了')
         }
@@ -68,7 +69,7 @@ router.get('/students/edit', (req, res) => {
     // 3. 渲染编辑页面
     //      根据 id 把学生信息查出来
     //      使用模板引擎渲染页面
-    fsHelp.findById(parseInt(req.query.id), (err, student) => {
+    Student.findById(req.query.id, (err, student) => {
         if (err) {
             return res.status(500).send('Server error.');
         }
@@ -83,9 +84,9 @@ router.post('/students/edit', (req, res) => {
     // 1. 获取表单数据
     //  req.body
     // 2. 更新
-    //  fsHelp.update()
+    //  student.update()
     // 3. 发送响应
-    fsHelp.updateById(req.body, (err) => {
+    student.updateById(req.body, (err) => {
         if (err) {
             console.log('保存失败了')
         }
@@ -102,7 +103,7 @@ router.get('/students/delete', (req, res) => {
     // 2. 根据 id 执行删除操作
     // 3. 根据操作结果发送响应数据
     console.log(req.query.id)
-    fsHelp.deleteById(req.query.id, (err) => {
+    student.deleteById(req.query.id, (err) => {
         if (err) {
             return res.status(500).send('Server error.');
         }
--- MongoDB/code/crud-express/student.js
+++ MongoDB/code/crud-express/student.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Lemon
  * @Date: 2020-01-19 17:13:43
- * @LastEditTime : 2020-01-19 17:21:18
+ * @LastEditTime : 2020-01-19 17:38:04
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Learning-Notes\MongoDB\code\crud-express\dbHelp.js
@@ -9,6 +9,7 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+// mongoose.connect('mongodb://localhost:27017/itcast');
 mongoose.connect('mongodb://localhost:27017/itcast');
 
 let studentSchema = new Schema({
@@ -25,7 +26,7 @@ let studentSchema = new Schema({
         type: Number,
         required: true
     },
-    hobbies: {
+    hobits: {
         type: String
     }
 })
