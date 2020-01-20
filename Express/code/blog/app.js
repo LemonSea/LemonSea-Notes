95f69412340a97b32b77baf368c798dca13b4c32
LemonSea <2533566560@qq.com>
2020-01-20 19:53:00 +0800

    静态资源开发配置

--- Express/code/blog/app.js
+++ Express/code/blog/app.js
@@ -1,16 +1,18 @@
 /*
  * @Author: your name
  * @Date: 2020-01-20 17:33:11
- * @LastEditTime : 2020-01-20 17:38:11
+ * @LastEditTime : 2020-01-20 19:32:19
  * @LastEditors  : Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Learning-Notes\Express\code\blog\app.js
  */
 let express = require('express');
+let path = require('path')
 
 let app = express();
 
-app.use('/public/', express.static('./public/'));
+app.use('/public/', express.static(path.join(__dirname, '/public/')));
+app.use('/node_modules/', express.static(path.join(__dirname, '/node_modules/')));
 
 app.get('/', (req, res) => {
     res.send('hello word');
