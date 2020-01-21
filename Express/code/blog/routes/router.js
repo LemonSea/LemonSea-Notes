027bdadb3d69643bea733f34f841c20bebcadf76
LemonSea <2533566560@qq.com>
2020-01-22 01:52:14 +0800

    处理登录请求

--- Express/code/blog/routes/router.js
+++ Express/code/blog/routes/router.js
@@ -61,12 +61,39 @@ router.post('/register', async (req, res) => {
     }
 })
 
+// 渲染登录页面
 router.get('/login', (req, res) => {
     res.render('login.html')
 })
 
+// 处理登录请求
 router.post('/login', (req, res) => {
+    let body = req.body;
+    User.findOne({
+        email: body.email,
+        password: md5(md5(body.password))
+    }, (err, user) => {
+        if (err) {
+            return res.status(500).json({
+                err_code: 500,
+                message: err.message
+            })
+        }
 
+        if (!user) {
+            return res.status(200).json({
+                err_code: 1,
+                message: 'Email or password is invalid!'
+            })
+        }
+
+        // 用户登录成功，记录登录状态
+        req.session.user = user;
+        return res.status(200).json({
+            err_code: 0,
+            message: 'OK'
+        })
+    })
 })
 
 module.exports = router;
