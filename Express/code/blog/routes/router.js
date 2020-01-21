0b1ee92c6951572259e13d97023ca8fcb938bc50
LemonSea <2533566560@qq.com>
2020-01-21 13:22:56 +0800

    用户模型

--- Express/code/blog/routes/router.js
+++ Express/code/blog/routes/router.js
@@ -13,7 +13,7 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-
+    console.log(req.body)
 })
 
 router.get('/login', (req, res) => {
--- Express/code/blog/routes/session.js
+++ Express/code/blog/routes/session.js
@@ -1,1 +1,29 @@
 // 注册、登录、退出
+let express = require('express');
+
+let router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('index.html', {
+        author: "lemon"
+    })
+})
+
+router.get('/register', (req, res) => {
+    res.render('register.html')
+})
+
+router.post('/register', (req, res) => {
+    console.log(req.body)
+})
+
+router.get('/login', (req, res) => {
+    res.render('login.html')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+
+module.exports = router;
