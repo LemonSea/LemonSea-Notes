8077af159faaf0d79bd55393c375c9e9aa692c35
LemonSea <2533566560@qq.com>
2020-01-23 22:57:05 +0800

    HMR

--- webpack/codes/Hello Webpack/src/index.js
+++ webpack/codes/Hello Webpack/src/index.js
@@ -1,1 +1,9 @@
-console.log('see you !')
+import './style.css';
+var btn = document.createElement('button');
+btn.innerHTML = 'add';
+document.body.appendChild(btn);
+btn.onclick = function () {
+    var div = document.createElement('div');
+    div.innerHTML = 'item';
+    document.body.appendChild(div);
+}
