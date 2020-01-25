9a4dc4fa1c4f709243c6b59a2c382c2470a48549
LemonSea <2533566560@qq.com>
2020-01-25 13:03:34 +0800

    trancform-runtime

--- webpack/codes/Hello Webpack/webpack.config.js
+++ webpack/codes/Hello Webpack/webpack.config.js
@@ -23,15 +23,7 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader',
-                options:
-                {
-                    "presets": [["@babel/preset-env", {
-                        useBuiltIns: "usage",
-                        corejs: 3
-                    }]]
-                }
-
+                loader: 'babel-loader'               
             },
             {
                 test: /\.(png|jpg|gif)$/,
