cab2d5b7143e4a92f0694fb95872f80bc6dc1f12
LemonSea <2533566560@qq.com>
2020-01-28 09:50:17 +0800

    pwa了解

---
+++ webpack/codes/Hello Webpack/dist/precache-manifest.9d38b72f0c4d271c681ec98d270fab07.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b32054bcd52217e4f7b7d00b6c479c65",
+    "url": "index.html"
+  },
+  {
+    "url": "main.93908fd0226f293deb18.js"
+  },
+  {
+    "url": "vendors~main.chunk.93908fd0226f293deb18.js"
+  },
+  {
+    "url": "vendors~main.chunk.93908fd0226f293deb18.js.LICENSE"
+  }
+]);
