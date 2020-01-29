898f22b0ac1983ff0daa636e0a469d5d86b13b8d
LemonSea <2533566560@qq.com>
2020-01-29 09:16:02 +0800

    单页面路由

--- webpack/codes/Hello Webpack/src/index.js
+++ webpack/codes/Hello Webpack/src/index.js
@@ -1,16 +1,49 @@
-import _ from 'loadsh';
+import 'regenerator-runtime/runtime';
 
-const dom = $('<div>');
-dom.html(_.join(['dell', 'lee']), ' ');
-$('body').append(dom);
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => {
-                console.log('service-worker register');
-            }).catch(error => {
-                console.log('serviec-work register error');
+
+import Home from './home';
+import List from './list';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: 'null'
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/react/api/demo.json')
+            .then(res => {
+                const data = res.data.data;
+                console.log(data)
+                this.setState({ data: JSON.stringify(data) });
             })
-    })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    {
+                        this.state.data
+                    }
+                </div>
+                <BrowserRouter>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/list' component={List} />
+                </BrowserRouter>
+            </div>
+        )
+    }
 }
+
+ReactDom.render(<App />, document.getElementById('root'));
