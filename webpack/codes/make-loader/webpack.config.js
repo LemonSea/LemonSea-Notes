c917e5294b2952e6bb24d1224fd22485f68bf287
LemonSea <2533566560@qq.com>
2020-01-29 23:18:47 +0800

    loader 设置

--- webpack/codes/make-loader/webpack.config.js
+++ webpack/codes/make-loader/webpack.config.js
@@ -5,13 +5,16 @@ module.exports = {
     entry: {
         main: './src/index.js'
     },
+    resolveLoader: {
+        modules: ['node_modules', './loaders']
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 use: [
                     {
-                        loader: path.resolve(__dirname, './loaders/replaceLoader.js'),
+                        loader: 'replaceLoader.js',
                         options: {
                             name: 'lee'
                         }
